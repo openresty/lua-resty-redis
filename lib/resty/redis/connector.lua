557f4fa2de0dc78e906b5a4f67e586a909827017
James Hurst <james@pintsized.co.uk>
2014-09-16 11:41:32 +0100

    Reworked return values so that you always get res, err plus sometimes previous_errors

--- lib/resty/redis/connector.lua
+++ lib/resty/redis/connector.lua
@@ -8,6 +8,7 @@ local ngx_log = ngx.log
 local ngx_DEBUG = ngx.DEBUG
 local ngx_ERR = ngx.ERR
 local tbl_insert = table.insert
+local tbl_remove = table.remove
 
 local ok, tbl_new = pcall(require, "table.new")
 if not ok then
@@ -59,40 +60,46 @@ end
 
 
 function _M.connect_via_sentinel(sentinels, master_name, try_slaves, options)
-    local s, err = _M.try_hosts(sentinels, options)
-    if s then
-        local master
-        master, err = sentinel.get_master(s, master_name)
-        if master then
-            return _M.connect_to_host(master, options)
+    local sentnl, err, previous_errors = _M.try_hosts(sentinels, options)
+    if not sentnl then
+        return nil, err, previous_errors
+    end
+
+    local master, err = sentinel.get_master(sentnl, master_name)
+    if master then
+        return _M.connect_to_host(master, options)
+    else
+        if not try_slaves then
+            return nil, err
         else
-            if try_slaves then
-                local slaves
-                slaves, err = sentinel.get_slaves(s, master_name)
-                if slaves then
-                    local slave
-                    slave, err = _M.try_hosts(slaves, options)
-                    if slave then
-                        return slave, nil
-                    end
-                end
+            local slaves, err = sentinel.get_slaves(sentnl, master_name)
+            if not slaves then
+                return nil, err
             end
+
+            local slave, err, previous_errors = _M.try_hosts(slaves, options)
+            if not slave then
+                return nil, err, previous_errors
+            end
+
+            return slave
         end
     end
-    return nil, err
 end
 
 
+-- In case of errors, returns "nil, err, previous_errors" where err is
+-- the last error received, and previous_errors is a table of the previous errors.
 function _M.try_hosts(hosts, options)
     local errors = tbl_new(#hosts, 0)
     for i, host in ipairs(hosts) do
         local r
         r, errors[i] = _M.connect_to_host(host, options)
         if r then
-            return r, errors
+            return r, tbl_remove(errors), errors
         end
     end
-    return nil, errors
+    return nil, tbl_remove(errors), errors
 end
 
 
@@ -108,7 +115,6 @@ function _M.connect_to_host(host, options)
     if socket then
         ok, err = r:connect(socket, options.connect_options)
     else
-        ngx_log(ngx_DEBUG, host.host, ":", host.port)
         ok, err = r:connect(host.host, host.port)
     end
 
